41996271db0563116fde13b4defe019ad580c89b
Kevaltech <158024906+Kevaltech@users.noreply.github.com>
2025-01-23 15:48:02 +0530

    dfd

--- src/components/CardDetails/CardContent.jsx
+++ src/components/CardDetails/CardContent.jsx
@@ -33,7 +33,7 @@ export const CardContent = ({ cardData, onStatusToggle, containerRef }) => {
 
     try {
       const response = await fetch(
-        `https://baf5-59-162-82-6.ngrok-free.app/?cardId=${cardData.cardId}&v1=${v1}&v2=${v2}`,
+        `https://baf5-59-162-82-6.ngrok-free.app/compare-cards/?cardId=${cardData.cardId}&v1=${v1}&v2=${v2}`,
         {
           headers: { "ngrok-skip-browser-warning": "234242" },
         }
