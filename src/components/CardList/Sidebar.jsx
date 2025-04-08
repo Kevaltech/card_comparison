7f8392b9f00a372787ffdb9bdbe79be54f9b6436
Kevaltech <158024906+Kevaltech@users.noreply.github.com>
2025-04-08 11:34:40 +0530

    dfdf

--- src/components/CardList/Sidebar.jsx
+++ src/components/CardList/Sidebar.jsx
@@ -178,7 +178,7 @@ export const Sidebar = ({
           <ul className="space-y-2 font-medium">
             <li className="px-0">
               <button
-                onClick={() => setOverview(!showOverview)}
+                onClick={() => setOverview(false)}
                 className="w-full flex items-center justify-between px-4 py-2 text-sm font-medium text-left text-gray-900 rounded-lg hover:bg-gray-100 D:hover:bg-gray-700 D:text-white D:hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
               >
                 <h6 className="text-lg font-bold D:text-white">Overview</h6>
